2b3902d127117bffa50f7716e9a2ad300ce38079
Gbemisola Odukoya <folaryz@gmail.com>
2022-05-26 23:11:43 +0100

    Added Styling components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react'
-import Product from './Product'
+import React from 'react';
+import Product from './Product';
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div>
       <h1>Hello world</h1>
+      <h2 className={styles.error}>An error occured</h2>
+
 
 <Product name='Amazon echo' description='Your AI Assistant' price={59.99} />
 <Product name='Iphone 12 pro max' description='The best iphone' price={1200} />
--- src/Product.jsx
+++ src/Product.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ItemDescription from './ItemDescription'
+import styles from "./Product.module.css";
+
 
 function Product({name, description, price}) {
   return (
-    <div>
+    <div className={styles.product}>
 
       
 
 <ItemDescription name={name} description={description}/>
-<h3>${price}</h3>
+<h2 className={styles.error}>An error occured</h2>
+<h3 className={styles.price}>${price}</h3>
     </div>
   )
 }
